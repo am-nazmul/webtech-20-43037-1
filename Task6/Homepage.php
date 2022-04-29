9bfaa2eddfb23c15fc34e4d5e640ab60f45869f9
am-nazmul <87535338+am-nazmul@users.noreply.github.com>
2022-04-29 15:08:31 +0600

    Add files via upload

---
+++ Task6/Homepage.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+    <style>
+        body
+{
+   
+    font-family:"Poppins", sans-serif;
+
+    height:100vh;
+    
+    background:url("Img/a.webp");
+    
+    height:100vh;
+    
+    -webkit-background-size: cover;
+    
+    background-size: cover;
+    
+    background-position: center center;
+    
+    position: relative; 
+   
+}
+a
+{
+    color: darkgrey;
+}
+
+.title{
+    color: #fa024d;
+}
+
+
+.bar{
+
+    display: inline-block;
+    position: absolute;
+    bottom: 30px;
+    right: 30px;
+    color: #cc6b16;
+
+}
+.main-title{
+    position: absolute;
+    display: block;
+    right: 40%;
+}
+.navigation
+{
+    display: block;
+    position: absolute;
+    min-width: 100%;
+    min-height: 60px;
+    background: #293740;
+}
+
+
+</style>
+</head>
+<body>
+    <div class="navigation">
+       
+        <h1 style="color: blanchedalmond; ">
+           <b class="main-title">Gadget Mart <sup>BD</sup></b> 
+        </h1>
+      </div> 
+      
+          <div class="bar">
+            <h3 align= "right">
+      
+      
+                <a class="title" href="Homepage.php">  Home |  </a> 
+                <a class="title" href="Login.php">  Login |  </a>  
+                <a class="title" href="Registration.php">  Registration </a> 
+          
+           
+            </h3>
+          </div>
+     
+    
+</body>
+</html>
+
+
+
+
+
+
